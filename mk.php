461d52aa5cd58401b19269cc4e62a5ecbbd898f2
tomascejka <dev.cejkatomas@gmail.com>
2012-12-18 14:09:08 +0100

    *repackage

--- mk.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-use Symfony\Component\Console\Application;
-use Mokos\Console\GenerateAllCommand;
-
-$console = new Application();
-$console->add(new GenerateAllCommand());
-$console->run();
